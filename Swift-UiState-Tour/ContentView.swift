2fb8fc5232cb2ebf94f56d229958abd167edc6f1
elias <eliaschen.dev@gmail.com>
2026-02-20 14:59:19 +0800

    Initial Commit

---
+++ Swift-UiState-Tour/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift-UiState-Tour
+//
+//  Created by Elias on 2026/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift-UiState-Tour/Swift_UiState_TourApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UiState_TourApp.swift
+//  Swift-UiState-Tour
+//
+//  Created by Elias on 2026/2/20.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UiState_TourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
